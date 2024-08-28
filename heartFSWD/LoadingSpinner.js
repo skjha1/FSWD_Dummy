3dfb83619244abf340e2cdb32e81dd9888a41124
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2024-08-28 20:17:49 +0530

    Create LoadingSpinner.js

---
+++ heartFSWD/LoadingSpinner.js
@@ -0,0 +1,13 @@
+// src/components/LoadingSpinner.js
+import React from 'react';
+import './LoadingSpinner.css';
+
+function LoadingSpinner() {
+    return (
+        <div className="spinner-container">
+            <div className="loading-spinner"></div>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
